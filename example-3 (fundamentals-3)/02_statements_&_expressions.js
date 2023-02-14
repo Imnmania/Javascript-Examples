5cb64cefbabd90e536c0ed96a867a5f1a32bf5de
Imnmania <niloyb@live.com>
2023-02-14 21:11:24 +0600

    statements and expressions

---
+++ example-3 (fundamentals-3)/02_statements_&_expressions.js
@@ -0,0 +1,16 @@
+/**
+** --------------------------
+** STATEMENTS AND EXPRESSIONS
+** --------------------------
+*/
+
+//? Expressions produce values
+// 3 + 4
+// 1991
+// true && false
+
+//? Statements does not produce values by itself (declarations/actions)
+//? it's a bigger piece of code
+// if (23 > 10) {
+     // do something
+// }
