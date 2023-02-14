d64010dfe0bab2ee67a23632c47ced574a9c6ac0
Imnmania <niloyb@live.com>
2023-02-14 21:19:12 +0600

    ternary operator

---
+++ example-3 (fundamentals-3)/03_ternary_operator.js
@@ -0,0 +1,11 @@
+/**
+** ----------------
+** TERNARY OPERATOR
+** ----------------
+*/
+
+const age = 23;
+// age >= 18 ? console.log('I like to drink wine') : console.log('I am underage');
+
+const drink = age >= 18 ? 'Wine 🍷' : 'Water 🚰';
+console.log(`I like to drink ${drink}`);
